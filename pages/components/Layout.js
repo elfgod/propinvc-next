b1a7fe0227aa6d78b82b433e02ef46f0bf7b38b7
ElfGod <albertogx@icloud.com>
2021-12-03 23:52:40 -0500

    Add Header Link

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { AppBar, Container, Toolbar, Typography, Link } from '@mui/material';
 import useStyles from '../../utils/styles';
 
-function Layout({ children, props }) {
+function Layout({ children }) {
   const classes = useStyles();
 
   return (
@@ -13,13 +14,26 @@ function Layout({ children, props }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>PropinVC</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>PropinVC</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
-      <foote className={classes.footer} r>
+      <footer className={classes.footer} r>
         <Typography>All rights reserved. @PropinVC</Typography>
-      </foote>
+      </footer>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Grid,
+  Typography,
+} from '@mui/material';
 import Layout from './components/Layout';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Propiedades</h1>
-        <ul>
-          <li>
-            Alquiler, cod:001, Cangrejo Apt, 60mts, 1 hab, 1 bañ, Linea blanca
-          </li>
-          <li>Alquiler, cod:002, Obarrio Apt, 70mts, 2 hab, 1 bañ, Amoblado</li>
-          <li>
-            Alquiler, cod:003, Ave balboa Apt, 80mts, 2 hab, 1 bañ, Linea blanca
-          </li>
-          <li>
-            Alquiler, cod:004, Bellavista Apt, 90mts, 2 hab, 1 bañ, Amoblado
-          </li>
-          <li>
-            Venta, cod:005, Marbella, Apt, 100mts, 2 hab, 1 bañ, Linea blanca
-          </li>
-          <li>
-            Venta, cod:006, San francisco, Apt, 110mts, 2 hab, 1 bañ, Amoblado
-          </li>
-          <li>
-            Venta, cod:007, Costa del este, Apt, 120mts, 2 hab, 1 bañ, linea
-            blanca
-          </li>
-          <li>Venta, cod:008, Bethania, Apt, 130mts, 2 hab, 1 bañ, Amoblado</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.properties.map((property) => (
+            <Grid item md={4} key={property.code}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={property.image}
+                    title={property.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{property.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${property.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
